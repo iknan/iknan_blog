b4b918b2706598f9955f6547e608ddfc411a4840
iknan <149158995+iknan@users.noreply.github.com>
2024-05-31 18:10:23 +0800

    s

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,8 @@
 import { defineUserConfig } from "vuepress";
+import { getDirname, path } from "vuepress/utils";
 import theme from "./theme.js";
+
+const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   base: "/",
@@ -16,6 +19,12 @@ export default defineUserConfig({
       title: "博客演示",
       description: "vuepress-theme-hope 的博客演示",
     },
+  },
+  alias: {
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue",
+    ),
   },
 
   theme,
