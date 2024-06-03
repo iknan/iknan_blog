9e674690a4aa4e8da7c6dd54595fc5d2ae56f1ba
iknan <149158995+iknan@users.noreply.github.com>
2024-06-03 15:35:48 +0800

    p

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,7 +1,6 @@
 import { hopeTheme } from "vuepress-theme-hope";
 import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
-import './styles/index.scss';
 export default hopeTheme({
   hostname: "https://mister-hope.github.io",
   
