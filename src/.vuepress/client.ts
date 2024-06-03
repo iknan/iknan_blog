45c068004afc436af56ef21176f6d67c4387bc83
iknan <149158995+iknan@users.noreply.github.com>
2024-06-03 16:12:29 +0800

    f

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,8 +1,13 @@
 import { defineClientConfig } from "vuepress/client";
 import { setupSnowFall } from "vuepress-theme-hope/presets/snowFall.js";
-
+import "vuepress-theme-hope/presets/shinning-feature-panel.scss"
+import "vuepress-theme-hope/presets/round-blogger-avatar.scss"
+import "vuepress-theme-hope/presets/hide-navbar-icon.scss"
+import "vuepress-theme-hope/presets/bounce-icon.scss"
+import "vuepress-theme-hope/presets/hr-driving-car.scss"
 export default defineClientConfig({
   setup() {
     setupSnowFall();
+    
   },
 });
